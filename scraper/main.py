1357eacc523d6b94fbba8aa64fb5ee5d732f7d70
Nicolas Nunez Manzano <nico.nm30@gmail.com>
2023-09-09 14:03:36 -0300

    funcion get_allReviews + Logia de logs y storeo

--- scraper/main.py
+++ scraper/main.py
@@ -6,6 +6,8 @@
 import json
 from scraper import GameScraper
 import os
+import time
+import random
 
 url = 'https://www.metacritic.com'
 
@@ -111,3 +113,60 @@ def get_details(bg_dict:dict):
 
         with open('./gd_scraped.txt', 'a') as scraped_txt:
             scraped_txt.writelines((str(game)+'\n'))
+        
+        time.sleep(random.randint(1, 10))
+
+def get_allReviews():
+    if not os.path.exists('./reviews.json'):
+        return "No hay usuarios para buscar."        
+
+    else:
+        with open('./reviews.json', 'r') as json_reviews:
+            users_dict = json.load(json_reviews)
+            users_list = list(users_dict.keys())    
+
+    if os.path.exists('./u_scraped.txt'):
+        with open('./u_scraped.txt', 'r') as scraped_txt:
+            users_ls = scraped_txt.readlines()
+            last_scraped = users_ls[-1].replace("\n","")
+
+            users_to_scrape = []
+
+            scrape_user = False
+
+            for user in users_list:
+
+                if scrape_user:
+                    users_to_scrape.append(user)
+
+                if user == last_scraped:
+
+                    scrape_user = True
+
+            users_list = users_to_scrape
+
+    else:
+        with open('./u_scraped.txt', 'w') as scraped_txt:
+            scraped_txt.writelines(('Usuarios scrapeados\n'))
+
+    games_reviewed = {}
+
+    for user in users_list:
+        
+        user_reviews,game_endpoint = gs.allReviews(user)
+        #TODO:Agregar las nuevas reviews al dict general "users_dict"
+
+        for game, endpoint in game_endpoint.items():
+            games_reviewed[game] = endpoint
+        #TODO: Guardar en JSON games_reviewed cada vez que termina el loop
+
+        with open('./reviews_2.json', 'w') as json_reviews:
+            json.dump(users_dict, json_reviews, indent=4)
+
+        with open('./u_scraped.txt', 'a') as scraped_txt:
+            scraped_txt.writelines((str(user)+'\n'))
+   
+    with open('./pending_details.json', 'w') as json_details:
+            json.dump(games_reviewed, json_details, indent=4)
+
+get_details(bg_dict)
